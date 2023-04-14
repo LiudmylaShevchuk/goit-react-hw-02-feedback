ba62ee132c0496337af6762e021db3b6957ab4a4
LiudmylaShevchuk <lashevchuk12@gmail.com>
2023-04-14 13:21:23 +0300

    App.jsx changed, components added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { Section } from './Section/Section';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Container } from './App.styled';
+
+export class App extends Component {
+  state = {
+  good: 0,
+  neutral: 0,
+  bad: 0,
+  };
+
+  render() {
+    const options = Object.keys(this.state);
+    return (
+      <Container>
+        <Section title="Please leave feedback">
+          <FeedbackOptions options={options} handleClick={this.handleClick} />
+        </Section>
+        <Section title="Statistics"></Section>
+      </Container>
+    );
+  }
+}
+
